fede527b78657b130dfe6b1dc1838858cdfd9f9e
ADEWUMIADENIKE <19peaceadewumi@gmail.com>
2022-09-10 21:27:49 -0700

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#incude <stdio.h>
 /**
  * main - entry point
  *
@@ -9,23 +9,23 @@ int main(void)
 	int i;
 	int j;
 
-		for (i = 0; i <= 99; i++)
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
 		{
-			for (j = i + 1; j <= 99; j++)
-			{
-				putchar(i / 10 + '0');
-				putchar(i % 10 + '0');
-				putchar(' ');
-				putchar(j / 10 + '0');
-				putchar(j / 10 + '0');
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
 
-				if (!(i == 98 && j == 99))
-				{
-					putchar(',');
-					putchar(' ');
-				}
+			if (!(i == 98 && j == 99))
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
+	}
 	putchar('\n');
 	return (0);
 }
