aed1dc8a05a5ffc3ce8d743147c9c7867a8b81cd
ADEWUMIADENIKE <19peaceadewumi@gmail.com>
2022-09-13 14:13:45 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *
+ * print_alphabet - function to print abc
+ *
+ * return: 0
+*/
+void print_alphabet(void)
+{
+	char C;
+	for (C = 'a'; C <= 'z'; C++)
+	{
+		_putchar(C);
+	}
+	_putchar('\n');
+}
