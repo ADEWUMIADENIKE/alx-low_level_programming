1f6f324e019dcad9404b711d30e1e82b18635afb
ADEWUMIADENIKE <19peaceadewumi@gmail.com>
2022-09-21 10:31:18 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
 
 /**
  * _strcat - concatenates two strings
- * @dest: string to pend to
+ * @dest: string to append to
  * @src: string to add
  *
  * Return: a pointer to the resulting string
@@ -15,14 +15,14 @@ char *_strcat(char *dest, char *src)
 	j = 0;
 
 	while (dest[i] != '\0')
-		i++
+		i++;
 
-			while (src[j] != '\0')
-			{
-				dest[i] = src[j];
-				j++;
-				i++;
-			}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
 
 	dest[i] = '\0';
 
