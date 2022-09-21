34b4a24e8da10f32c102b03cd8345a966161d02b
ADEWUMIADENIKE <19peaceadewumi@gmail.com>
2022-09-21 13:05:35 -0700

    8

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,7 +2,7 @@
 
 /**
  * print_number - prints an integer;
- * @n: inteer to be printed;
+ * @n: integer to be printed;
 */
 void print_number(int n)
 {
@@ -17,7 +17,7 @@ void print_number(int n)
 		n1 = n;
 	}
 
-	if (n1/ 10)
+	if (n1 / 10)
 		print_number(n1 / 10);
 
 	_putchar((n1 % 10) + '0');
