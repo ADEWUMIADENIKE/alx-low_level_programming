9d8863c4925a3e3be213febc910f6a71f38295f6
ADEWUMIADENIKE <19peaceadewumi@gmail.com>
2022-09-28 11:53:30 -0700

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,4 +1,4 @@
-#incllud "main.h"
+#include "main.h"
 
 int bandersnatch(char *s1, char *s2);
 char *move(char *s2);
@@ -41,7 +41,7 @@ int wildcmp(char *s1, char *s2)
 	 * if we reached the end of both strings,
 	 * return 0
 	 */
-	if (*s1  '\0' || *s2 == '\0')
+	if (*s1 == '\0' || *s2 == '\0')
 		return (0);
 
 	/**
@@ -74,36 +74,4 @@ int wildcmp(char *s1, char *s2)
  *
  * Return: return value of wildcmp() or of itself
  */
-int bandersnatch(char *s1, char *s2)
-{
-	/**
-	 * if we reached the end of s1, return o
-	 * if chars are equal, return the return value of the wildcmp()
-	 * increament s1 by 1, not s2
-	 */
-	if (*s1 == '\0')
-		return (0);
-	if (*s1 == *s2)
-		return (wildcmp(s1, s2));
-	return (bandersnatch(s1 + 1, s2));
-}
-
-/**
- * *move - moves the current char past the *
- * @s2: string to iterate over
- *
- * Return: the address of the character after the *
- */
-char *move(char *s2)
-{
-	/**
-	 * if the current char is a *
-	 * increment s2 by 1
-	 * else return the address of
-	 * the first char past all *
-	 */
-	if (*s2 == '*')
-		return (move(s2 + 1));
-	else
-		return (s2);
-}
+int bandersnatch
