9401b2fab776c7b95210568a0f5abaf24145b49a
ADEWUMIADENIKE <19peaceadewumi@gmail.com>
2022-11-01 08:47:10 -0700

    io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -44,7 +44,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 		len = write(fd, text_content, _strlen(text_content));
 	close(fd);
-	if (len == -1)
+	if (len == -1);
 		return (-1);
 	return (1);
 }
